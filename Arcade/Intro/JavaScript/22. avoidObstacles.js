b0046eea208a428fa70c68347ff0fb5411c85d67
Kamronbek Shodmonov <kamronbek.swe28@gmail.com>
2022-07-21 17:43:32 +0500

    updated arcade/intro solution

--- Arcade/Intro/JavaScript/22. avoidObstacles.js
+++ Arcade/Intro/JavaScript/22. avoidObstacles.js
@@ -1,5 +1,5 @@
 /*
-  O(n * m) time | O(1) space - where n is the length of the input array and m is the answer
+  O(n * m) time | O(1) space - where n is the length of the input array and m is the output
 */
 
 function solution(inputArray) {
