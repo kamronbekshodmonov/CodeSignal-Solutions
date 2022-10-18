2fb9148bae906160e9d2b632861be3e227b26d18
Kamronbek Shodmonov <kamronbek.swe28@gmail.com>
2022-10-19 00:59:36 +0500

    updated arcade/intro solution

--- Arcade/Intro/JavaScript/53. validTime.js
+++ Arcade/Intro/JavaScript/53. validTime.js
@@ -3,8 +3,7 @@
 */
 
 function solution(time) {
-  const [hour, minut] = time.split(":");
-  if (!(0 <= Number(hour) && Number(hour) <= 23)) return false;
-  if (!(0 <= Number(minut) && Number(minut) <= 59)) return false;
+  const [hours, minutes] = time.split(":").map((value) => Number(value));
+  if (hours >= 24 || minutes >= 60) return false;
   return true;
 }
